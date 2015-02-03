b3c2fe881717e3bf5097e03c2040a67423f784a4
Ashlynn Pai <ashlynnpai@gmail.com>
2015-02-03 16:38:21 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+    <!-- Main jumbotron -->
+    <div class="jumbotron">
+      <div class="container-fluid">
+          <div id="header-image">
+       
+      </div>
+    </div>
+
+     <!-- Rows -->
+
+    <div class="container-fluid">
+      
+     </div>  
+             
+              
+
+<?php get_footer(); ?>
---
+++ single.php
@@ -0,0 +1,46 @@
+<?php get_header(); ?>
+
+<div class="container-fluid">   
+      <div class="row">     
+        <div class="blog-photo">
+          <h1 id='blog-description'><?php bloginfo('description'); ?></h1>
+        </div>
+          <div class="col-md-9" id="single-blog">
+
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <div class="page-header">      
+
+    <h1><?php the_title(); ?></h1>
+            <p>
+              By <?php the_author(); ?> 
+              on <?php echo the_time('l, F jS, Y');?>
+              in <?php the_category( ', ' ); ?>.
+              <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a>
+            </p>
+            </div>
+      
+          <p><?php the_content(); ?></p>
+          <p><?php the_tags(); ?></p>          
+                
+    <div class="next-posts"><?php next_post_link('%link', 'Newer entry &gt;') ?></div>
+    <div class="prev-posts"><?php previous_post_link('%link', '&lt; Older entry') ?></div>
+
+		<?php comments_template( '', true ); ?> 
+        
+           <?php endwhile; else: ?>
+          
+          <div class="page-header">
+            <h1>Oh no!</h1>
+          </div>
+
+          <p>No content is appearing for this page!</p>
+
+        <?php endif; ?>
+     
+        </div>
+       
+      <?php get_sidebar( 'blog' ); ?>
+ </div>
+</div>
+
+<?php get_footer(); ?>
