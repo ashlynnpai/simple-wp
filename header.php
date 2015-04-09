d97fd3bebc889b2e4a6c714d39e03811aef3100e
Ashlynn Pai <ashlynnpai@gmail.com>
2015-04-09 00:27:15 -0400

    responsive images

--- header.php
+++ header.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link href='http://fonts.googleapis.com/css?family=Quicksand|Montserrat|Allura' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Playball' rel='stylesheet' type='text/css'>
      <link rel="shortcut icon" href="<?php bloginfo('template_directory');?>/images/favicon.ico">
 
     <title>
@@ -18,13 +19,17 @@
 
   <body <?php body_class(); ?>>
 
-    <div class="nav navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="nav navbar navbar-inverse navbar-fixed-top navbar-default" role="navigation">
       <div class="container-fluid">
         <nav>
-         <ul>
-          
-         </ul>   
-        </nav>
+          <ul>
+            <li><a href="http://www.ashlynnpai.com/work">MY WORK</a></li>
+            <li><a href="http://www.ashlynnpai.com/blog" class="selected">BLOG</a></li>
+            <li><a href="http://www.ashlynnpai.com" id="name">Ashlynn Pai</a></li>
+            <li><a href="http://www.ashlynnpai.com/about">ABOUT</a></li>
+            <li><a href="http://www.ashlynnpai.com/blog/contact/">CONTACT</a></li>
+          </ul>   
+          </nav>
         </div><!--/.navbar-collapse -->
 
       </div>
