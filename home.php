a72af1610cf813440f90cb948525e488677f199e
Ashlynn Pai <ashlynnpai@gmail.com>
2015-06-24 07:22:23 -0400

    home.php change query

--- home.php
+++ home.php
@@ -2,13 +2,8 @@
 
   <div class="container-fluid">   
     <div class="row" id="blog-list">
-      <div class="blog-photo">
-        <h1 id='blog-description'><?php bloginfo('description'); ?></h1>
-      </div>
-        <div class="col-md-9">
-            <h1 id="blog-head"><?php wp_title(''); ?></h1>
-
-      <?php query_posts('showposts=5'); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <div class="col-md-9">
+        <?php query_posts('showposts=10'); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
           <article class="post">
             
@@ -42,5 +37,5 @@
       <?php get_sidebar( 'blog' ); ?>
 
     </div>
-</div>
+  </div>
 <?php get_footer(); ?>
