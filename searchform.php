9217cd384ef547340de321f3961d12e216eea4f3
ashlynnpai <ashlynnpai@gmail.com>
2016-09-03 20:13:12 -0400

    add searchform.php to style search

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+    <div>
+        <input type="text" value="" placeholder="Search" name="s" id="s">
+        <span><i class="glyphicon glyphicon-search"></i></span> 
+    </div>
+</form>
+
